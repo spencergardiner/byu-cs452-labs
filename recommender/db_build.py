0fd3259a4e4d1322496b6fadb408687d6889c2db
spencergardiner <spencer.gardiner@gmail.com>
2024-10-12 13:14:55 -0600

    build db

--- recommender/db_build.py
+++ recommender/db_build.py
@@ -6,21 +6,39 @@ import psycopg2
 
 load_dotenv()
 
-CONNECTION = None # paste connection string here or read from .env file
+CONNECTION = os.getenv("CONNECTION_STRING")
 
 # need to run this to enable vector data type
 CREATE_EXTENSION = "CREATE EXTENSION vector"
 
 # TODO: Add create table statement
 CREATE_PODCAST_TABLE = """
-
+CREATE TABLE podcast(
+    id TEXT PRIMARY KEY,
+    title TEXT
+);
 """
 # TODO: Add create table statement
 CREATE_SEGMENT_TABLE = """
-
+CREATE TABLE podcast_segment(
+    id TEXT PRIMARY KEY,
+    start_time FLOAT,
+    end_time FLOAT,
+    content TEXT,
+    embedding VECTOR(128),
+    podcast_id  TEXT REFERENCES podcast(id)
+);
 """
 
+# COMPLETED: Create tables with psycopg2 (example: https://www.geeksforgeeks.org/executing-sql-query-with-psycopg2-in-python/)
 conn = psycopg2.connect(CONNECTION)
-# TODO: Create tables with psycopg2 (example: https://www.geeksforgeeks.org/executing-sql-query-with-psycopg2-in-python/)
+conn.autocommit = True
 
+cursor = conn.cursor()
 
+# cursor.execute(CREATE_EXTENSION)
+cursor.execute(CREATE_PODCAST_TABLE)
+cursor.execute(CREATE_SEGMENT_TABLE)
+
+cursor.close()
+conn.close()
